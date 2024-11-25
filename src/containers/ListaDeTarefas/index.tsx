8c77f8fbf2e462515f486fef9d54ff374c66d016
Pedro Ribeiro <pedrorocha@gmail.com>
2024-11-25 17:52:10 -0300

    estlzação avança!

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,6 +1,25 @@
+import Tarefa from '../../components/FiltroCard/Tarefa'
+
 const ListaDeTarefas = () => (
   <main>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
   </main>
 )
 
