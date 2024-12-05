cd0a01c2e5f9f9a96c36c2685bc88299f340c1ef
Pedro Ribeiro <pedrorocha@gmail.com>
2024-12-05 17:39:01 -0300

    Começando o redux!

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import BarraLateral from './containers/BarraLateral'
 import EstiloGlobal, { Container } from './styles'
 import ListaDeTarefas from './containers/ListaDeTarefas'
+import { Provider } from 'react-redux'
+
+import store from './store'
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <EstiloGlobal />
       <Container>
         <BarraLateral />
         <ListaDeTarefas />
       </Container>
-    </>
+    </Provider>
   )
 }
 
