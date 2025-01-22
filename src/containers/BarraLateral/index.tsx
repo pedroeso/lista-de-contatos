ddee18170beacd73b0b7bc2e9ddea637b151c84c
Pedro Ribeiro <pedrorocha@gmail.com>
2025-01-22 19:17:37 -0300

    avançando

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -7,47 +7,57 @@ import * as enums from '../../utils/enums/Tarefa'
 import { Campo } from '../../styles'
 import { RootReducer } from '../../store'
 
-const BarraLateral = () => {
+type Props = {
+  mostrarFiltros: boolean
+}
+
+const BarraLateral = ({ mostrarFiltros }: Props) => {
   const dispatch = useDispatch()
   const { termo } = useSelector((state: RootReducer) => state.filtro)
 
   return (
     <S.Aside>
       <div>
-        <Campo
-          type="text"
-          placeholder="Buscar"
-          value={termo}
-          onChange={(evento) => dispatch(alternaTermo(evento.target.value))}
-        />
-        <S.Filtros>
-          <FiltroCard
-            valor={enums.Status.PENDENTE}
-            criterio="status"
-            legenda="pendentes"
-          />
-          <FiltroCard
-            valor={enums.Status.CONCLUIDA}
-            criterio="status"
-            legenda="concluídas"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.URGENTE}
-            criterio="prioridade"
-            legenda="urgentes"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.IMPORTANTE}
-            criterio="prioridade"
-            legenda="importantes"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.NORMAL}
-            criterio="prioridade"
-            legenda="normal"
-          />
-          <FiltroCard criterio="todas" legenda="todas" />
-        </S.Filtros>
+        {mostrarFiltros ? (
+          <>
+            <Campo
+              type="text"
+              placeholder="Buscar"
+              value={termo}
+              onChange={(evento) => dispatch(alternaTermo(evento.target.value))}
+            />
+            <S.Filtros>
+              <FiltroCard
+                valor={enums.Status.PENDENTE}
+                criterio="status"
+                legenda="pendentes"
+              />
+              <FiltroCard
+                valor={enums.Status.CONCLUIDA}
+                criterio="status"
+                legenda="concluídas"
+              />
+              <FiltroCard
+                valor={enums.Prioridade.URGENTE}
+                criterio="prioridade"
+                legenda="urgentes"
+              />
+              <FiltroCard
+                valor={enums.Prioridade.IMPORTANTE}
+                criterio="prioridade"
+                legenda="importantes"
+              />
+              <FiltroCard
+                valor={enums.Prioridade.NORMAL}
+                criterio="prioridade"
+                legenda="normal"
+              />
+              <FiltroCard criterio="todas" legenda="todas" />
+            </S.Filtros>
+          </>
+        ) : (
+          <button type="button">Voltar a lista de tarefas</button>
+        )}
       </div>
     </S.Aside>
   )
